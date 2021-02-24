889b074442ce75cec40ddff4e0d2b944172d5e51
BettyHoPro <bettyvisualtech@gmail.com>
2021-02-24 04:07:28 +0000

    refactory the jquery inside the composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
   // counting how many texts to fill out in the new tweet textarea
-  $(".tweet-text").on("keyup", function(event) {
-    let countNum =  $(this).val();
-    if (countNum.length > 140) {
-      // when the number is greater than 140, count number go red
-      $(".counter").addClass("count-red");
-    } else if (countNum.length < 140) {
-      //when the number is less than 140, count number goes black
-      $(".counter").removeClass("count-red");
-    }
-    // syn remain text number which allows to fill to front-end
-    $(".counter").html(140 - countNum.length);
-  });
+  $(".tweet-text").on("keyup", keyupEvent);
 });
+
+
+const keyupEvent = function(event) {
+  let countNum =  $(this).val();
+  if (countNum.length > 140) {
+    // when the number is greater than 140, count number go red
+    $(".counter").addClass("count-red");
+  } else if (countNum.length < 140) {
+    //when the number is less than 140, count number goes black
+    $(".counter").removeClass("count-red");
+  }
+  // syn remain text number which allows to fill to front-end
+  $(".counter").html(140 - countNum.length);
+};
